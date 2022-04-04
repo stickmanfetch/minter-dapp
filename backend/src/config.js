eda197a92a0e4ceb1ba20893c7102693e9c62aa0
Stickman Fetch <stickman.alliance@gmail.com>
2022-04-04 20:22:01 +0800

    asdasd

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,7 +59,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x07b99965E59d89683DBbbc83fc74786f42897F08';
 const TREASURY_ADDRESS = '0x03AdC733EF4690c891cD8f7fD625981f41473196';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 100; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = .1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
