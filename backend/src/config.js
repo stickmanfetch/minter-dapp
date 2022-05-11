5afd0ea14320323046e7ac61c4055cb3b7f487ad
Stickman Fetch <stickman.alliance@gmail.com>
2022-05-12 01:23:48 +0800

    time

--- backend/src/config.js
+++ backend/src/config.js
@@ -135,7 +135,7 @@ const ROYALTY_SHARE = 750; // Percentage of the token price that goes to the roy
 const ROYALTY_ADDRESS = "0x6C4B387883ffE8fC0bD043585ad75ED856e459C7"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = [""]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "0x472166853404eb47604d8bc03746b50005c44efa"; // If you want to manually include it
