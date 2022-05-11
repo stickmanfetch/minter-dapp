4fc6b07e8b9839365c7480a8bd89ce484a9ed380
Stickman Fetch <stickman.alliance@gmail.com>
2022-05-12 01:26:00 +0800

    contract update

--- backend/src/config.js
+++ backend/src/config.js
@@ -130,7 +130,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-08-12T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-08-10T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-08-11T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 750; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x6C4B387883ffE8fC0bD043585ad75ED856e459C7"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
